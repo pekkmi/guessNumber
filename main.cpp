b9e5d11099cf071a2b1fb6109b31ad26e013d4d0
pekkmi <pekkmi@gmail.com>
2025-01-11 19:55:50 +0200

    Added comments for the final code

--- main.cpp
+++ main.cpp
@@ -4,32 +4,38 @@
 
 using namespace std;
 
+// Prototype
 int game(int maxnum);
-
 
 
 int main()
 {
+    // declare variables for times you have guessed and maximum number to guess
     int numberOfGuesses = 0;
     int maxnum = 40;
+    // call game() function with the parameter maxnum
     numberOfGuesses = game(maxnum);
 
 
 }
 
+    // Game logic in its own function
 int game(int maxnum) {
 
+    // Declare variables for game logic
     int rNumber = 0;
     int guess = 0;
     int numberOfGuesses = 0;
 
 
+    // Random generate a number that is between 0 and maxnumber
     srand(time(0));
 
+    // maxnumber + 1 because if maxnum = 40 it randomizes number between 0 and 39
     rNumber = rand() % maxnum +1;
 
 
-
+    // Making a while loop for checkin if guessed number is the same as randomized number
     while (true) {
 
 
@@ -39,20 +45,23 @@ int game(int maxnum) {
 
         if (guess == rNumber) {
             cout << "GZ! You are correct!" << endl;
-
+            // Guess is same as randomized number and the loop ends.
             break;
         }
 
+            // Check if guessed number is between the right parametres
         if (guess < 0 || guess > maxnum) {
             cout << "Try to guess number between 0 and " << maxnum << "." << endl;
 
         }
 
+            // Check if the number you guessed is smaller than the number randomized
         if (guess < rNumber) {
             cout << "Your guess is smaller than the number i am thinking of. Try again!" << endl;
 
         }
 
+            // Check if the number you guessed is larger than the number randomized
         if (guess > rNumber) {
             cout << "Your guess is larger than the number i am thinking of. Try again!" << endl;
 
@@ -61,16 +70,21 @@ int game(int maxnum) {
 
 
     }
+        // Print the amount you guessed
     cout << "You guessed " << numberOfGuesses << " times" << endl;
+
+        // Ask if you want to play again or quit
     char newGame;
     cout << "do you want to play a new game? Y/N" << endl;
     cin >> newGame;
 
     if (newGame == 'y') {
+        // Return back to start of the game
         return game(maxnum);
     }else {
         return 0;
     }
+        //Return numberOfGuesses to main() function
     return numberOfGuesses;
 }
 
